8a45ff1f5dc8a4f0d78004c5b8ae46a5e0c8d551
GDA47 <bechir.jp@gmail.com>
2025-05-07 09:50:08 +0200

    Add files via upload

---
+++ Conseil Agricole.py
@@ -0,0 +1,9 @@
+# fichier : app.py
+
+import streamlit as st
+
+st.set_page_config(page_title="Bienvenue", page_icon="👋")
+
+st.title("👋 Bienvenue sur mon application Streamlit !")
+
+st.write("Ceci est une petite application d'exemple en français.")
