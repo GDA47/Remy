69934777b8016c0f6ad7930fb649c68de961a281
GDA47 <bechir.jp@gmail.com>
2025-05-07 10:10:37 +0200

    Update Conseil Agricole.py

--- Conseil Agricole.py
+++ Conseil Agricole.py
@@ -3,7 +3,3 @@
 import streamlit as st
 
 st.set_page_config(page_title="Bienvenue", page_icon="👋")
-
-st.title("👋 Bienvenue sur mon application Streamlit !")
-
-st.write("Ceci est une petite application d'exemple en français.")
